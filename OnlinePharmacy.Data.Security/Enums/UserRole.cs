29521f75dac12151a2d3a34733a1418e39195b1f
Sean Greaves <sean.greaves@thermofisher.com>
2023-11-15 14:23:17 -0500

    New phase

--- OnlinePharmacy.Data.Security/Enums/UserRole.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ScriptEase.OnlinePharmacy.Data.Security.Enums
-{
-    public enum UserRole
-    {
-        Customer,  // Browse drugs, add to cart, make purchases.
-        Admin,     // Manage users, update inventory, view sales reports.
-        Pharmacist // Update drug information, manage inventory.
-    }
-}
